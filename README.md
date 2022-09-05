935a453404ff4a4528f5b82846b6861e7e72861a
vdkarthi <68495846+vdkarthi@users.noreply.github.com>
2022-09-05 17:01:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-84uu4h-jgxzcn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-84uu4h-jgxzcn)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../products'
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+
+export class ProductAlertsComponent implements OnInit {
+
+  constructor() { }
+  @Input() product!: Product;
+  ngOnInit(): void {
+  }
+
+}
